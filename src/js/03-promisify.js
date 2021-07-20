d4574e2fd43bb126299ff454063ec048d942ae73
Rodion Romanenko <rod.romanenko@gmail.com>
2021-07-20 16:28:24 +0400

    Update 03-promisify.js

--- src/js/03-promisify.js
+++ src/js/03-promisify.js
@@ -1,13 +1,37 @@
 // Subtask 1, Delay function
-const delay = ms => {
-    return new Promise(resolve => {
-        setTimeout(() => resolve(ms), ms);
-    });
-};
+// const delay = ms => {
+//     return new Promise(resolve => {
+//         setTimeout(() => resolve(ms), ms);
+//     });
+// };
 
 const logger = time => console.log(`Fulfilled after ${time}ms`);
 
 // Tests
-delay(2000).then(logger); // Fulfilled after 2000ms
-delay(1000).then(logger); // Fulfilled after 1000ms
-delay(1500).then(logger); // Fulfilled after 1500ms
+// delay(2000).then(logger); // Fulfilled after 2000ms
+// delay(1000).then(logger); // Fulfilled after 1000ms
+// delay(1500).then(logger); // Fulfilled after 1500ms
+
+
+//Subtask 2, toggleUserState function refactoring
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: false },
+];
+
+const toggleUserState = (allUsers, username, callback) => {
+  const updatedUsers = allUsers.map(user =>
+    user.name === username ? { ...user, active: !user.active } : user
+  );
+
+  callback(updatedUsers);
+};
+
+// Currently the function works like this
+toggleUserState(users, 'Mango', console.table);
+toggleUserState(users, 'Ajax', console.table);
+
+// The function should work like this
+// toggleUserState(users, 'Mango').then(console.table);
+// toggleUserState(users, 'Ajax').then(console.table);
